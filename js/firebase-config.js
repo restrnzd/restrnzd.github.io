7b13c452a47c7965663ff26bf5a18887143b328b
restrnzd <155936734+restrnzd@users.noreply.github.com>
2024-01-08 09:10:37 -0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Reemplaza con la configuración de tu proyecto de Firebase
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
