481b04d4e637881d9a16538b72b3ce0f47ea94d6
restrnzd <155936734+restrnzd@users.noreply.github.com>
2024-01-08 13:17:52 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,30 +1,45 @@
-// Obtén una referencia al objeto de autenticación de Firebase
-const auth = firebase.auth();
+// Obtén una referencia al botón de inicio de sesión con Google
+var googleLoginButton = document.getElementById('google-login');
 
-// Configura los botones de inicio de sesión de Google y Facebook
-document.getElementById('google-login').addEventListener('click', () => signInWithGoogle());
-document.getElementById('facebook-login').addEventListener('click', () => signInWithFacebook());
+// Añade un evento click al botón de Google
+googleLoginButton.addEventListener('click', signInWithGoogle);
 
 // Función para iniciar sesión con Google
 function signInWithGoogle() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  signInWithProvider(provider);
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      var user = result.user;
+      console.log("Usuario autenticado con Google:", user);
+      redirectToSesionPage(user); // Redirige a la página de sesión con los datos del usuario
+    })
+    .catch((error) => {
+      console.error("Error al autenticar con Google:", error);
+    });
 }
+
+// Obtén una referencia al botón de inicio de sesión con Facebook
+var facebookLoginButton = document.getElementById('facebook-login');
+
+// Añade un evento click al botón de Facebook
+facebookLoginButton.addEventListener('click', signInWithFacebook);
 
 // Función para iniciar sesión con Facebook
 function signInWithFacebook() {
-  const provider = new firebase.auth.FacebookAuthProvider();
-  signInWithProvider(provider);
-}
-
-// Función común para iniciar sesión con proveedores
-function signInWithProvider(provider) {
-  auth.signInWithPopup(provider)
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider)
     .then((result) => {
-      const user = result.user;
-      console.log(`Usuario autenticado: ${user.displayName}`);
+      var user = result.user;
+      console.log("Usuario autenticado con Facebook:", user);
+      redirectToSesionPage(user); // Redirige a la página de sesión con los datos del usuario
     })
     .catch((error) => {
-      console.error(error.message);
+      console.error("Error al autenticar con Facebook:", error);
     });
+}
+
+// Función para redirigir a la página de sesión con los datos del usuario
+function redirectToSesionPage(user) {
+  localStorage.setItem('user', JSON.stringify(user)); // Almacena los datos del usuario en el almacenamiento local
+  window.location.href = 'sesion.html';
 }
