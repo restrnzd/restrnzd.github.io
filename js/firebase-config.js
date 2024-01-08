82608822a8544e951826912a324bdba1644d13c6
restrnzd <155936734+restrnzd@users.noreply.github.com>
2024-01-08 12:59:15 -0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Reemplaza con la configuración de tu proyecto de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBS9Iy01nGihdBZcVcX26sdYG6qS_2EBWQ",
+  authDomain: "rest-zavala.firebaseapp.com",
+  projectId: "rest-zavala",
+  storageBucket: "rest-zavala.appspot.com",
+  messagingSenderId: "789112399672",
+  appId: "1:789112399672:web:066a317555f49780fb83cd"
+};
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
