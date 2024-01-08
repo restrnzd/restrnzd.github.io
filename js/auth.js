e74474aa8a5188d7d6a2d0f7efa29ef02f150d36
restrnzd <155936734+restrnzd@users.noreply.github.com>
2024-01-08 12:32:55 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,6 @@
 // Obtén una referencia al objeto de autenticación de Firebase
 const auth = firebase.auth();
 
-// Configura los botones de inicio de sesión de Google y Facebook
-document.getElementById('google-login').addEventListener('click', () => signInWithGoogle());
-document.getElementById('facebook-login').addEventListener('click', () => signInWithFacebook());
-
 // Función para iniciar sesión con Google
 function signInWithGoogle() {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -23,21 +19,6 @@ function signInWithProvider(provider) {
     .then((result) => {
       const user = result.user;
       console.log(`Usuario autenticado: ${user.displayName}`);
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
-}
-// ...
-
-// Función común para iniciar sesión con proveedores
-function signInWithProvider(provider) {
-  auth.signInWithPopup(provider)
-    .then((result) => {
-      const user = result.user;
-      console.log(`Usuario autenticado: ${user.displayName}`);
-
-      // Redireccionar o mostrar el formulario después de autenticar
       showAuthenticatedContent(user);
     })
     .catch((error) => {
@@ -45,7 +26,19 @@ function signInWithProvider(provider) {
     });
 }
 
-// Función para mostrar el contenido después de autenticar
+// Función para cerrar sesión
+function signOut() {
+  auth.signOut()
+    .then(() => {
+      console.log("Sesión cerrada.");
+      showLoginContent();
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+}
+
+// Función para mostrar contenido después de autenticar
 function showAuthenticatedContent(user) {
   // Ocultar el contenido de inicio de sesión
   document.getElementById('login-container').style.display = 'none';
@@ -59,3 +52,16 @@ function showAuthenticatedContent(user) {
   document.getElementById('avatar').src = user.photoURL || 'ruta_default_avatar.jpg';
 }
 
+// Función para mostrar contenido de inicio de sesión
+function showLoginContent() {
+  // Ocultar el formulario y contenido después de cerrar sesión
+  document.getElementById('sesion-container').style.display = 'none';
+
+  // Mostrar el contenido de inicio de sesión
+  document.getElementById('login-container').style.display = 'block';
+}
+
+// Configura los botones de inicio de sesión de Google y Facebook
+document.getElementById('google-login').addEventListener('click', () => signInWithGoogle());
+document.getElementById('facebook-login').addEventListener('click', () => signInWithFacebook());
+document.getElementById('terminarSesion').addEventListener('click', () => signOut());
