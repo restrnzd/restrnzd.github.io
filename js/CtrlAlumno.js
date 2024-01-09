08b82913f030cee3a41b269ef0da5d795679d715
restrnzd <155936734+restrnzd@users.noreply.github.com>
2024-01-08 18:31:11 -0600

    Update CtrlAlumno.js

--- js/CtrlAlumno.js
+++ js/CtrlAlumno.js
@@ -50,10 +50,10 @@ async function busca() {
           import("./tipos.js").
                   Alumno} */
       const data = doc.data();
-      forma.rnombre.value = data.rnombre;
-      forma.rapellido.value = data.rapellido || "";
+      forma.matricula.value = data.matricula;
+      forma.nombre.value = data.nombre || "";
       forma.telefono.value = data.telefono || "";
-      forma.rhora.value = data.rhora || "";
+      forma.grupo.value = data.grupo || "";
       forma.fecha.value = data.fecha || "";
       forma.addEventListener(
         "submit", guarda);
@@ -76,21 +76,21 @@ async function guarda(evt) {
     evt.preventDefault();
     const formData =
       new FormData(forma);
-    const rnombre = getString(
-        formData, "rnombre").trim();  
-    const rapellido = getString(formData, "rapellido").trim();
+    const matricula = getString(
+        formData, "matricula").trim();  
+    const nombre = getString(formData, "nombre").trim();
     const telefono = getString(formData, "telefono").trim();
-    const rhora = getString(formData, "rhora").trim();
+    const grupo = getString(formData, "grupo").trim();
     const fecha = getString(formData, "fecha").trim();
     /**
      * @type {
         import("./tipos.js").
                 Alumno} */
     const modelo = {
-      rnombre, 
-      rapellido,
+      matricula, 
+      nombre,
       telefono,
-      rhora,
+      grupo,
       fecha
     };
     await daoAlumno.
